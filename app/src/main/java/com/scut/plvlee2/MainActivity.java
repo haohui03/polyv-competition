482bf4d52593f246227350837fbed8fd3d1b3f34
lee <lee@qq.com>
2022-11-17 21:23:33 +0800

    空activity

---
+++ app/src/main/java/com/scut/plvlee2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.scut.plvlee2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.*;
+import android.os.Bundle;
+
+import com.easefun.polyv.livecommon.module.config.PLVLiveSDKConfig;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/scut/plvlee2/PLVapp.java
@@ -0,0 +1,17 @@
+package com.scut.plvlee2;
+
+import android.app.Application;
+
+import com.easefun.polyv.livecommon.module.config.PLVLiveSDKConfig;
+
+public class PLVapp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PLVLiveSDKConfig.init(
+                new PLVLiveSDKConfig.Parameter(this)//sdk初始化所需参数
+                        .isOpenDebugLog(true)//是否打开调试日志
+                        .isEnableHttpDns(false)//是否使用httpdns//
+        );
+    }
+}
