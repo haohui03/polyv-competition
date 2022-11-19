09701919ce9d723af4168476d71faee4835afea4
lee <lee@qq.com>
2022-11-19 19:19:35 +0800

    引入StreamActivity和模块

--- app/src/main/java/com/scut/plvlee2/MainActivity.java
+++ app/src/main/java/com/scut/plvlee2/MainActivity.java
@@ -2,17 +2,45 @@ package com.scut.plvlee2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.*;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.easefun.polyv.livecommon.module.config.PLVLiveChannelConfig;
 import com.easefun.polyv.livecommon.module.config.PLVLiveSDKConfig;
+import com.easefun.polyv.livecommon.module.data.IPLVLiveRoomDataManager;
+import com.easefun.polyv.livecommon.module.data.PLVLiveRoomDataManager;
+import com.easefun.polyv.livecommon.module.modules.streamer.contract.IPLVStreamerContract;
+import com.easefun.polyv.livecommon.module.modules.streamer.presenter.PLVStreamerPresenter;
+import com.plv.livescenes.config.PLVLiveChannelType;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PLVLiveChannelConfig ConFig;
+
+    private IPLVLiveRoomDataManager LiveRoomManage ;
+
+    private IPLVStreamerContract.IStreamerPresenter presnter;
+
+    void init(){
+        ConFig = new PLVLiveChannelConfig();
+        ConFig.setChannelType(PLVLiveChannelType.SEMINAR);
+        LiveRoomManage = new  PLVLiveRoomDataManager(ConFig);
+        presnter = new PLVStreamerPresenter(LiveRoomManage);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        startActivity(new Intent(this,LoginStreamerActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
---
+++ polyvLiveStreamerScene/src/main/java/com/easefun/polyv/livestreamer/modules/chatroom/IPLVLSChatroomLayout.java
@@ -0,0 +1,111 @@
+package com.easefun.polyv.livestreamer.modules.chatroom;
+
+import com.easefun.polyv.livecommon.module.data.IPLVLiveRoomDataManager;
+import com.easefun.polyv.livecommon.module.utils.listener.IPLVOnDataChangedListener;
+
+/**
+ * 聊天室布局的接口定义
+ * 定义了：
+ * 1、外部直接调用的方法
+ * 2、需要外部响应的事件监听器
+ */
+public interface IPLVLSChatroomLayout {
+
+    // <editor-fold defaultstate="collapsed" desc="1、外部直接调用的方法">
+    /**
+     * 初始化
+     *
+     * @param liveRoomDataManager 直播间数据管理器
+     */
+    void init(IPLVLiveRoomDataManager liveRoomDataManager);
+
+    /**
+     * 设置view交互事件监听器
+     *
+     * @param listener 监听器
+     */
+    void setOnViewActionListener(OnViewActionListener listener);
+
+    /**
+     * 添加聊天室在线人数变化监听器
+     *
+     * @param listener 监听器
+     */
+    void addOnOnlineCountListener(IPLVOnDataChangedListener<Integer> listener);
+
+    /**
+     * 设置麦克风开关按钮状态
+     *
+     * @param isOpen true：打开，false：关闭
+     */
+    void setOpenMicViewStatus(boolean isOpen);
+
+    /**
+     * 设置摄像头开关按钮状态
+     *
+     * @param isOpen true：打开，false：关闭
+     */
+    void setOpenCameraViewStatus(boolean isOpen);
+
+    /**
+     * 设置前置摄像头按钮状态
+     *
+     * @param isFront true：前置，false：后置
+     */
+    void setFrontCameraViewStatus(boolean isFront);
+
+    /**
+     * 通知文档区域标注工具栏展开状态
+     *
+     * @param isExpand 是否展开
+     */
+    void notifyDocumentMarkToolExpand(boolean isExpand);
+
+    /**
+     * 通知文档布局全屏状态
+     *
+     * @param isFullScreen 是否全屏显示
+     */
+    void notifyDocumentLayoutFullScreen(boolean isFullScreen);
+
+    /**
+     * 是否拦截返回事件
+     *
+     * @return true：拦截，false：不拦截
+     */
+    boolean onBackPressed();
+
+    /**
+     * 销毁，释放资源
+     */
+    void destroy();
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="2、需要外部响应的事件监听器">
+    /**
+     * view交互事件监听器
+     */
+    interface OnViewActionListener {
+        /**
+         * 麦克风控制
+         *
+         * @param isMute true：禁用，false：启用
+         */
+        boolean onMicControl(boolean isMute);
+
+        /**
+         * 摄像头控制
+         *
+         * @param isMute true：禁用，false：启用
+         */
+        boolean onCameraControl(boolean isMute);
+
+        /**
+         * 前置摄像头控制
+         *
+         * @param isFront true：前置，false：后置
+         */
+        boolean onFrontCameraControl(boolean isFront);
+    }
+    // </editor-fold>
+}
