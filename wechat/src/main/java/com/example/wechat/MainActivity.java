88b1e5ac584e08f1b2b6de656950345481e1f47d
gjh <2583936090@qq.com>
2022-11-25 11:24:12 +0800

    fragment的使用

--- wechat/src/main/java/com/example/wechat/MainActivity.java
+++ wechat/src/main/java/com/example/wechat/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.wechat;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        msgFragment mf = new msgFragment();
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-//        ft.add(android.R.id.content, mf);
+//        msgFragment mf = new msgFragment();
+//        FragmentTransaction ft = getFragmentManager().beginTransaction();
+//        ft.add(android.R.id.mid, mf);
 //        ft.commit();
-        Log.i("MainActivity", "smsb");
+
+        ImageView msgBtn = (ImageView) findViewById(R.id.msgBtn);
+        ImageView bookBtn = (ImageView) findViewById(R.id.bookBtn);
+        ImageView discoveryBtn = (ImageView) findViewById(R.id.discoveryBtn);
+        ImageView meBtn = (ImageView) findViewById(R.id.meBtn);
+        msgBtn.setOnClickListener(l);
+        bookBtn.setOnClickListener(l);
+        discoveryBtn.setOnClickListener(l);
+        meBtn.setOnClickListener(l);
+
     }
+    View.OnClickListener l = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            Fragment fragment = new Fragment();
+            switch (view.getId()) {
+                case R.id.msgBtn:
+                    fragment = new msgFragment();
+
+                    break;
+                case R.id.bookBtn:
+                    break;
+                case R.id.discoveryBtn:
+                    break;
+                case R.id.meBtn:
+                    break;
+                default:break;
+            }
+            ft.replace(R.id.mid, fragment);
+            ft.commit();
+        }
+    };
 }
--- wechat/src/main/java/com/example/wechat/msgFragment.java
+++ wechat/src/main/java/com/example/wechat/msgFragment.java
@@ -1,12 +1,16 @@
 package com.example.wechat;
 
+import android.app.Fragment;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class msgFragment extends Fragment {
     private int[] imgArray = new int[] {
@@ -25,10 +29,25 @@ public class msgFragment extends Fragment {
     };
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_msg, container, false);
+        Button chat_btn =  view.findViewById(R.id.chat_btn);
+        chat_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "点击了msgFragment的聊天按钮", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
+
     }
+
+
 }
